c63cf34ed70817b9a3eac2b09bd71c7b28fcb561
Nikhil <nikhilchowdarygarapati@gmail.com>
2025-08-06 00:46:31 +0530

    05: Feed3 Stories 2

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -3,11 +3,13 @@ import { dummyStoriesData } from "../assets/assets";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import { StoryModal } from "./StoryModal";
+import { StoryViewer } from "./StoryViewer";
 
 export const StoriesBar = () => {
 
     const [stories, setStories] = useState([])
     const [showModal, setShowModal] = useState(false)
+    const [viewStory, setViewStory] = useState(null)
 
     const fetchStories = async () => {
         setStories(dummyStoriesData)
@@ -32,7 +34,7 @@ export const StoriesBar = () => {
             {/* Story Cards */}
             {
                 stories.map((story, index) => (
-                    <div key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}>
+                    <div onClick={() => setViewStory(story)} key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}>
                         <img src={story.user.profile_picture} alt="" className="absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow" />
                         <p className="absolute top-18 left-3 text-white/60 text-sm truncate max-w-24">{story.content}</p>
                         <p className="text-white absolute bottom-1 right-2 z-10 text-xs">{moment(story.createdAt).fromNow()}</p>
@@ -55,7 +57,8 @@ export const StoriesBar = () => {
 
         {/* Add story modal */}
         {showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories}/>}
-
+        {/* View Story Modal */}
+        {viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory} />}
     </div>
   );
 };
