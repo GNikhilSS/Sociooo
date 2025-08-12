9208635104fc16e5f3c6c7a23d65ae91275329a1
Nikhil <nikhilchowdarygarapati@gmail.com>
2025-08-12 12:11:24 +0530

    16: Inngest func + vercel setup

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -15,9 +15,55 @@ const syncUserCreation = inngest.createFunction(
         // Check username existence
         const user = await User.findOne({username})
 
+        if (user) {
+            username = username + Math.floor(Math.random() * 10000)
+        }
+
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url, 
+            username
+        }
+
+        await User.create(userData)
         
     }
 )
 
+// Ingest Function to update user data to a database
+const syncUserUpdation = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        
+        const updateUserData = {
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url
+        }
+
+        await User.findByIdAndUpdate(id, updateUserData)
+        
+    }
+)
+
+// Ingest Function to delete user data to a database
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-with-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ({event})=>{
+        const {id} = event.data
+        
+        await User.findByIdAndDelete(id)
+    }
+)
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+];
